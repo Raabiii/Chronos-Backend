0f96a1ae862210a7beb4d81a0ff59100dcb52ffb
tobsich_fabian <fabian.tobisch@htlpinkafeld.at>
2025-01-13 15:57:18 +0100

    changed test file

--- test-env.ts
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-
-console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
-console.log('SUPABASE_KEY:', process.env.SUPABASE_KEY);
-console.log('JWT_SECRET:', process.env.JWT_SECRET);
